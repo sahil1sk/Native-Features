27f13c82668d18213a50cea3aff4f5f73a757d52
sahil1sk <khannasahil303@gmail.com>
2020-12-01 11:40:51 +0530

    Adding Location Input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
+import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -22,55 +24,54 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
   }
 
   void _savePlace() {
-    if(_titleController.text.isEmpty || _pickedImage == null) return;
-    Provider.of<GreatPlaces>(
-      context, 
-      listen: false
-    ).addPlace(
+    if (_titleController.text.isEmpty || _pickedImage == null) return;
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
       _titleController.text,
       _pickedImage,
     );
     Navigator.of(context).pop(); // after adding the place we pop this screen
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add a New Place'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch, // for taking the full screen width
-        children: <Widget>[
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: _titleController,
-                    ),
-                    SizedBox(height: 10),
-                    ImageInput(_selectImage),
-                  ],
+        appBar: AppBar(
+          title: Text('Add a New Place'),
+        ),
+        body: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.stretch, // for taking the full screen width
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: _titleController,
+                      ),
+                      SizedBox(height: 10),
+                      ImageInput(_selectImage),
+                      SizedBox(height: 10),
+                      LocationInput(),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          RaisedButton.icon(
-            icon: Icon(Icons.add),
-            label: Text('Add Place'),
-            textColor: Colors.white,
-            onPressed: _savePlace,
-            elevation: 0,
-            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // will shrink the size of text according to the button inside text
-            color: Theme.of(context).accentColor,
-          ),
-        ],
-      )
-    );
+            RaisedButton.icon(
+              icon: Icon(Icons.add),
+              label: Text('Add Place'),
+              textColor: Colors.white,
+              onPressed: _savePlace,
+              elevation: 0,
+              materialTapTargetSize: MaterialTapTargetSize
+                  .shrinkWrap, // will shrink the size of text according to the button inside text
+              color: Theme.of(context).accentColor,
+            ),
+          ],
+        ));
   }
 }
